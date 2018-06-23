ec155bd0cca70b4e4988d7f5eba6e08bd776a410
Dmitry Logashenko <LogashenkoDL@mail.ru>
2018-06-23 15:19:41 +0300

    A cosmetic TeX correction in a doxygen comment.

--- fv1/convection_diffusion_fv1.h
+++ fv1/convection_diffusion_fv1.h
@@ -52,8 +52,8 @@ namespace ConvectionDiffusionPlugin{
  * assemblings for the convection diffusion equation.
  * The Equation has the form
  * \f[
- * 	\partial_t (m1*c + m2) - \nabla \left( D \nabla c - \vec{v} c \right - \vec{F}) +
- * 		r1 \cdot c + r2 = f + f2
+ * 	\partial_t (m1*c + m2) - \nabla \left( D \nabla c - \vec{v} c - \vec{F} \right )
+ * 		+ r1 \cdot c + r2 = f + f2
  * \f]
  * with
  * <ul>
